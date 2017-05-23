dc90ca0612c16e09a8a1a224eab97df328a67fc4
Jesse Snyder <jsnyderdesign@outlook.com>
2017-05-23 11:27:27 -0700

    Add files via upload

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * redmond Theme Customizer
+ *
+ * @package redmond
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function redmond_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'redmond_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function redmond_customize_preview_js() {
+	wp_enqueue_script( 'redmond_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'redmond_customize_preview_js' );
